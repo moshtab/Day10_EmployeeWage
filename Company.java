3e5e0ef301191a5a7c83a2d044bad3542742a044
moshtab <mdmohsin41439@gmail.com>
2021-08-09 00:54:08 +0530

    Displaying Total Empwage of multiple companies

--- Company.java
+++ Company.java
@@ -14,11 +14,10 @@ class EmployeeWage {
 		this.noOfWorkingDays = noOfWorkingDays;
 	}
 
-	public void employeeWage() {
+	public int employeeWage() {
 		int empHrs = 0;
 		int totalWorkingDays = 0;
 		int workingHr = 0;
-		int dailyEmpWage = 0;
 		int empWage = 0;
 
 		while (empHrs <= maxHrs && totalWorkingDays <= noOfWorkingDays) {
@@ -39,13 +38,13 @@ class EmployeeWage {
 			}
 			}
 			empHrs += workingHr;
-			dailyEmpWage = workingHr * wagePerHr;
+
 			empWage = empWage + (workingHr * wagePerHr);
 			totalWorkingDays++;
-			System.out.print(dailyEmpWage + " ");
+
 		}
-		System.out.println();
-		System.out.println("Total Employee wage for 20 days is : " + empWage);
+
+		return empWage;
 
 	}
 }
@@ -60,16 +59,12 @@ class Company {
 		EmployeeWage company3 = new EmployeeWage(18, 95, 18);
         
 		System.out.println();
-		System.out.println("Daily EmployeeWage of Company1 is");
-		company1.employeeWage();
-       
-		System.out.println();
-		System.out.println("Daily EmployeeWage of Company2 is");
-		company2.employeeWage();
-        
-		System.out.println();
-		System.out.println("Daily EmployeeWage of Company3 is");
-		company3.employeeWage();
+		System.out.println("Total Employee Wage of Company1 is : " + company1.employeeWage());
+
+		System.out.println("Total Employee Wage of Company2 is : " + company2.employeeWage());
+
+		
+		System.out.println("Total Employee Wage of Company3 is : " + company3.employeeWage());
 
 	}
 
