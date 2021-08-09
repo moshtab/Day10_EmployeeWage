7ea46ad5aaa7a13c9627d91534f0308d074ab734
moshtab <mdmohsin41439@gmail.com>
2021-08-09 09:58:45 +0530

    Separated classes

--- Company.java
+++ Company.java
@@ -1,53 +1,6 @@
 package employeeWage;
 
-import java.util.Random;
 
-class EmployeeWage {
-
-	public int wagePerHr;
-	public int maxHrs;
-	public int noOfWorkingDays;
-
-	public EmployeeWage(int wagePerHr, int maxHrs, int noOfWorkingDays) {
-		this.wagePerHr = wagePerHr;
-		this.maxHrs = maxHrs;
-		this.noOfWorkingDays = noOfWorkingDays;
-	}
-
-	public int employeeWage() {
-		int empHrs = 0;
-		int totalWorkingDays = 0;
-		int workingHr = 0;
-		int empWage = 0;
-
-		while (empHrs <= maxHrs && totalWorkingDays <= noOfWorkingDays) {
-			Random r = new Random();
-			int empCheck = r.nextInt(3);
-			switch (empCheck) {
-			case 0: {
-				workingHr = 8;
-				break;
-			}
-			case 1: {
-				workingHr = 4;
-				break;
-			}
-			case 2: {
-				workingHr = 0;
-				break;
-			}
-			}
-			empHrs += workingHr;
-
-			empWage = empWage + (workingHr * wagePerHr);
-			totalWorkingDays++;
-
-		}
-
-		return empWage;
-
-	}
-}
 
 class Company {
 
@@ -57,14 +10,13 @@ class Company {
 		EmployeeWage company1 = new EmployeeWage(20, 100, 20);
 		EmployeeWage company2 = new EmployeeWage(22, 110, 22);
 		EmployeeWage company3 = new EmployeeWage(18, 95, 18);
-        
+
 		System.out.println();
 		System.out.println("Total Employee Wage of Company1 is : " + company1.employeeWage());
 
 		System.out.println("Total Employee Wage of Company2 is : " + company2.employeeWage());
 
-		
-		System.out.println("Total Employee Wage of Company3 is : " + company3.employeeWage());
+		System.out.println("Total Employee Wagee of Company3 is : " + company3.employeeWage());
 
 	}
 
